a92e3829aa78bbadf8596f0a3f5d1b4eb36332fb
Reijan Lopes <reijan.lj@gmail.com>
2023-06-27 15:16:13 -0300

    add middleware and create server_graphql-ws

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,61 @@
 import Koa from "koa";
 import koaCors from "koa-cors";
+import bodyParser from "koa-bodyparser";
+import convert from "koa-convert";
+
 import { connect } from "mongoose";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { WebSocketServer } from "ws";
+import { execute, subscribe } from "graphql";
+
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  sendResult,
+  shouldRenderGraphiQL,
+} from "graphql-helix";
+
+import { schema } from "./schema/index";
 
 import "dotenv/config";
-import { WebSocketServer } from "ws";
-
-const app = new Koa();
-app.use(koaCors);
 
 const mongoDB = process.env.MONGODB_CONNECT as string;
 const port = process.env.PORT;
+
+const app = new Koa();
+app.use(convert(koaCors()));
+app.use(convert(bodyParser()));
+
+app.use(
+  convert(async (ctx, next) => {
+    const request = {
+      body: ctx.request.body,
+      headers: ctx.req.headers,
+      method: ctx.request.method,
+      query: ctx.request.query,
+    };
+
+    await next();
+    if (shouldRenderGraphiQL(request)) {
+      ctx.body = renderGraphiQL({
+        subscriptionsEndpoint: `ws://localhost:${port}`,
+      });
+    } else {
+      const { operationName, query, variables } = getGraphQLParameters(request);
+
+      const result = await processRequest({
+        operationName,
+        query,
+        variables,
+        request,
+        schema,
+      });
+
+      sendResult(result, ctx.res);
+    }
+  })
+);
 
 connect(mongoDB)
   .then(() => {
@@ -22,5 +68,7 @@ const server = app.listen(port, () => {
     server,
     path: "/",
   });
+
+  useServer({ schema, execute, subscribe }, wsServer);
   console.log(`Server running, ${port}`);
 });
