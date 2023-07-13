4634120272ca43561094a6fce58ee5dd49be6f8d
Reijan Lopes <reijan.lj@gmail.com>
2023-07-13 11:51:41 -0300

    starting the frontend, setting up the environment and fixing bugs

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,46 +1,51 @@
 export type UserInput = {
-  _id: string;
-  name: string;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-  cards: [string];
-  debts: [string];
-};
+  _id: string
+  name: string
+  email: string
+  createdAt: string
+  updatedAt: string
+  cards: [string]
+  debts: [string]
+}
 
 export type CardInput = {
-  _id: string;
-  name: string;
-  number: string;
-  cpf: string;
-  expiration: string;
-  cvv: number;
-  createdAt: string;
-  updatedAt: string;
-  debts: [string];
-  user: string;
-};
+  _id: string
+  name: string
+  number: string
+  cpf: string
+  expiration: string
+  cvv: number
+  createdAt: string
+  updatedAt: string
+  debts: [string]
+  user: string
+}
+
+
+
+
+
 
 export type DebtInput = {
-  _id: string;
-  value: number;
-  cashback: number;
-  numberOfInstallments: number;
-  totalValue: number;
-  cet: number;
-  createdAt: string;
-  updatedAt: string;
-  user: string;
-  card: string;
-  tax: string;
-  installments: [InstallmentsInput];
-};
+  _id: string
+  value: number
+  cashback: number
+  numberOfInstallments: number
+  totalValue: number
+  cet: number
+  createdAt: string
+  updatedAt: string
+  user: string
+  card: string
+  tax: string
+  installments: [InstallmentsInput]
+}
 
 export type InstallmentsInput = {
-  status: string;
-  idMonth: number;
-  value: number;
-  expires: string;
-  createdAt: string;
-  updatedAt: string;
-};
+  status: string
+  idMonth: number
+  value: number
+  expires: string
+  createdAt: string
+  updatedAt: string
+}
